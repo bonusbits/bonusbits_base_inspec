e305f4d2bbb42e5c8f0c03fc2e8a67251bade3bd
Levon Becker <levon.becker.github@bonusbits.com>
2017-04-04 11:37:14 -0700

    It's am Amazon Linux package...

--- controls/yum_cron.rb
+++ controls/yum_cron.rb
@@ -1,7 +1,7 @@
 require_relative '../helpers/os_queries'
 configure_yum_cron = attribute('configure_yum_cron', default: true, description: 'Configure Yum Cron')
 
-if configure_yum_cron && rhel_family?
+if configure_yum_cron && amazon?
   describe 'Yum Cron' do
     it 'yum-cron installed' do
       expect(package('yum-cron')).to be_installed
